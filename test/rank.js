2c15254960256df851499b92fb16526f98eabb5d
BenM <magyar@american.edu>
2014-03-27 23:23:52 -0400

    Setup of Makefile and tests for standard ranking

--- test/rank.js
+++ test/rank.js
@@ -1,19 +1,41 @@
 var expect = require('expect.js');
-var ranker = require('../lib/rank.js');
+var rank = require('../lib/rank.js');
 
-describe('Fractional Ranking', function() {
-	it('should provide the the standard ranking when no duplicates', function(){
-		expect(ranker.fractional([{ x : 1},{x : 2},{x : 3}], 'x')).
+describe('Standard Ranking', function(){
+	it('should rank ascending order for nonduplicates', function(){
+		expect(rank.standard([{ x : 1},{x : 3},{x : 2}], 'x')).
 			to.eql([{ x : 1, rank : 1},{x : 2, rank : 2},{x : 3, rank : 3}]);
 	});
 
+	it('should rank asending order even if there are duplicates', function(){
+		expect(rank.standard([{ x : 1},{x : 3},{x : 1}], 'x')).
+			to.eql([{ x : 1, rank : 1},{x : 1, rank : 2},{x : 3, rank : 3}]);
+	});
+
+	it('should provide the same ranking no matter the order', function(){
+	expect(rank.standard([{ x : 1},{x : 3},{x : 1}], 'x')).
+				to.eql(rank.standard([{ x : 3},{x : 1},{x : 1}], 'x'));
+		});
+});
+
+describe('Fractional Ranking', function(){
 	it('should provide the the standard ranking when no duplicates', function(){
-		expect(ranker.fractional([{ x : 2},{x : 1},{x : 3}], 'x')).
+		expect(rank.fractional([{ x : 1},{x : 2},{x : 3}], 'x')).
 			to.eql([{ x : 1, rank : 1},{x : 2, rank : 2},{x : 3, rank : 3}]);
+	});
+
+	it('should not provide the the standard ranking when there are duplicates', function(){
+		expect(rank.fractional([{ x : 2},{x : 2},{x : 3}], 'x')).
+			not.to.eql(rank.standard([{ x : 2},{x : 2},{x : 3}], 'x'));
 	});
 
 	it('should provide handle floats', function(){
-		expect(ranker.fractional([{ x : 1.23},{x : 3.123},{x : 2.43}], 'x')).
+		expect(rank.fractional([{ x : 1.23},{x : 3.123},{x : 2.43}], 'x')).
 			to.eql([{ x : 1.23, rank : 1},{x : 2.43, rank : 2},{x : 3.123, rank : 3}]);
+	});
+
+	it('should provide the same rank for same valued elements', function(){
+		expect(rank.fractional([{x : 1},{x : 1},{x : 1}], 'x')).
+			to.eql([{x : 1, rank : 2},{x : 1, rank : 2},{x : 1, rank : 2}]);
 	});
 });
